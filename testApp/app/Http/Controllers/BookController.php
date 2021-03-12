9813df0d88ae085f8375234ee6958a32516e90a0
Samy30 <samibelaid30@gmail.com>
2021-03-13 00:21:33 +0100

    first commit

---
+++ testApp/app/Http/Controllers/BookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return Book::all();
+    }
+ 
+    public function show($id)
+    {
+        return Book::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Book::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->update($request->all());
+
+        return $book;
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return 204;
+    }
+}
